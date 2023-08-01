7da3868feb74ef2a42a7f8a3414599b3dce46fe5
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2023-08-01 10:32:18 +0700

    change image url

--- src/Component/Home/MostOrdered.js
+++ src/Component/Home/MostOrdered.js
@@ -24,7 +24,7 @@ export default function MostOrdered() {
       <View style={styles.container}>
         <ImageBackground
           source={{
-            uri: "https://images.unsplash.com/photo-1690232260627-bcd8d9352973?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=986&q=80",
+            uri: "https://images.unsplash.com/photo-1517677208171-0bc6725a3e60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
           }}
           style={styles.imgBackground}
         >
